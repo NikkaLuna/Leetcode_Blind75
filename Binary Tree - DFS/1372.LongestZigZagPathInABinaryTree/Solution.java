535b1f0c4c737268763fe0434812a67757c264c2
Andrea Hayes <ahaye74@wgu.edu>
2024-05-20 09:59:35 -0500

    Update Solution

--- Binary Tree - DFS/1372.LongestZigZagPathInABinaryTree/Solution.java
+++ Binary Tree - DFS/1372.LongestZigZagPathInABinaryTree/Solution.java
@@ -16,11 +16,14 @@ class Solution {
     }
 
     private void dfs(TreeNode node, boolean isLeft, int length) {
+
         if (node == null) return;
         maxLength = Math.max(maxLength, length);
+
         if (isLeft) {
             dfs(node.left, false, length + 1);
             dfs(node.right, true, 1);
+            
         } else {
             dfs(node.right, true, length + 1);
             dfs(node.left, false, 1);
